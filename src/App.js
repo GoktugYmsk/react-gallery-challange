0e86beff0228b9859be5919d6dcc0d8e6c190a2b
GoktugYmsk <gogtug033@outlook.com>
2023-04-20 16:20:58 +0300

    car names arranged with grid

--- src/App.js
+++ src/App.js
@@ -5,31 +5,29 @@ import { initialData } from './assets/data';
 function App() {
   const [list, setList] = useState(initialData);
   const [selectedCar, setSelectedCar] = useState(true);
-  const [selectedInfo, setSelectedInfo] = useState(false)
 
   const handleCarClick = (id) => {
     const selectedCar = list.find(car => car.id === id);
     setSelectedCar(selectedCar);
     console.log('Selected car:', selectedCar);
-    setSelectedInfo(true)
   }
 
   return (
     <div className="App">
       <h1 className='h1' >Hyundai Cars</h1>
       <div className="carList">
-        {list.map(car => (
-          <div className="car" key={car.id} onClick={() => handleCarClick(car.id)}>
+        {list.map((car, index) => (
+          <div className={`car ${index % 4 === 0 ? 'firstInRow' : ''}`} key={car.id} onClick={() => handleCarClick(car.id)}>
             <h3 className='carName' >{car.title}</h3>
           </div>
         ))}
       </div>
-      <div className="carDetails">
-        <h2 className='selectedCarTitle'>{selectedCar.title}</h2>
-        {selectedInfo && (
+      {selectedCar && (
+        <div className="carDetails">
+          <h2 className='selectedCarTitle'>{selectedCar.title}</h2>
           <p>{selectedCar.content}</p>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
