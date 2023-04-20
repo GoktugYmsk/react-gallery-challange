bffac9604d2ed48dc8a9dbd066474c8bdecaac01
GoktugYmsk <gogtug033@outlook.com>
2023-04-20 16:59:43 +0300

    url set based on selected vehicle info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 import { initialData } from './assets/data';
 
 function App() {
   const [list, setList] = useState(initialData);
-  const [selectedCar, setSelectedCar] = useState(true);
+  const [selectedCar, setSelectedCar] = useState(null);
+  const navigate = useNavigate();
 
   const handleCarClick = (id) => {
     const selectedCar = list.find(car => car.id === id);
     setSelectedCar(selectedCar);
-    console.log('Selected car:', selectedCar);
+    navigate(`/${selectedCar.title}`);
   }
 
   return (
     <div className="App">
-      <h1 className='h1' >Hyundai Cars</h1>
+      <h1 className='h1'>Hyundai Cars</h1>
       <div className="carList">
         {list.map((car, index) => (
-          <div className={`car ${index % 4 === 0 ? 'firstInRow' : ''}`} key={car.id} onClick={() => handleCarClick(car.id)}>
-            <h3 className='carName' >{car.title}</h3>
+          <div className="car" key={car.id} onClick={() => handleCarClick(car.id)}>
+            <h3 className='carName'>{car.title}</h3>
           </div>
         ))}
       </div>
