6ed12f15ba7eba3ebf4e5bfdaa11500de017c549
GoktugYmsk <gogtug033@outlook.com>
2023-04-21 21:56:20 +0300

    css fixed

--- src/components/car/index.js
+++ src/components/car/index.js
@@ -21,16 +21,19 @@ function Car({ handleCarClick, selectedCar, list, setSelectedCar, navigate }) {
           <title> Hyundai </title>
         </Helmet>
         {closeCarPage &&
-          <>
+          <div className='carInfoPage' >
             <div className="carList">
               {list.map(car => (
                 <div className="car" key={car.id} onClick={() => handleCarClick(car.id)}>
                   <h3 className='carName'>{car.title}</h3>
                 </div>
               ))}
-            </div><p>Daha Fazla Bilgi için</p>
-            <button onClick={goToDetail}>Devam et</button>
-          </>
+            </div>
+            <div className='detailPageObject' >
+              <p>Daha Fazla Bilgi için</p>
+              <button onClick={goToDetail}>Devam et</button>
+            </div>
+          </div>
         }
         {selectedCar && (
           <div className="carDetails">
@@ -39,9 +42,8 @@ function Car({ handleCarClick, selectedCar, list, setSelectedCar, navigate }) {
         )}
       </div>
       {!closeCarPage &&
-        <Detail navigate={navigate} setcloseCarPage={setcloseCarPage} />
+        <Detail navigate={navigate} setcloseCarPage={setSelectedCar} />
       }
-
     </>
   );
 }
