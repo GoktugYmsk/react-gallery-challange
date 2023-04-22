a38068ad1b8a97add419fa4558ffa851aaf5dbdd
GoktugYmsk <gogtug033@outlook.com>
2023-04-22 11:03:43 +0300

    css fixed

--- src/components/detailPopup/index.js
+++ src/components/detailPopup/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
-
 import { productList } from '../../assets/data';
 import './index.css';
 
@@ -16,42 +14,38 @@ function Detail({ setcloseCarPage }) {
   };
 
   return (
-    <div>
+    <div className='detailContainer'>
       <Helmet>
         <title> Car Details </title>
       </Helmet>
       {vehicle.map(info => (
         <div className='detailVehicle' key={info.productId}>
           <table>
-            <tbody>
+            <div className='infoDetail' >
               <tr>
-                <td colSpan='2'>{info.productName}</td>
+                <td colSpan='4'>{info.productName}</td>
               </tr>
               <tr>
                 <td>Model</td>
                 <td>{info.model}</td>
-              </tr>
-              <tr>
                 <td>Trim Code</td>
                 <td>{info.trimCode}</td>
               </tr>
               <tr>
                 <td>Motor Gücü</td>
                 <td>{info.enginePower}</td>
-              </tr>
-              <tr>
                 <td>Vites Türü</td>
                 <td>{info.transmission}</td>
               </tr>
               <tr>
                 <td>Renk Seçenekleri</td>
-                <td>{info.color}</td>
+                <td colSpan='3'>{info.color}</td>
               </tr>
-            </tbody>
+            </div>
           </table>
         </div>
       ))}
-      <button onClick={comeBackMainPage}>Anasayfa</button>
+      <button className='mainPageBackButton' onClick={comeBackMainPage}>Anasayfa</button>
     </div>
   );
 }
