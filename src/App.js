1ac5ebd2eaa6d19e64d94531b73e8cb638dd5ca6
GoktugYmsk <gogtug033@outlook.com>
2023-04-22 10:37:48 +0300

    detail page fixed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from 'react';
 import { Routes, useNavigate, Route } from 'react-router-dom';
 
 import { initialData } from './assets/data';
+import { productList } from './assets/data';
 import Car from './components/car';
 import './App.css';
 
 function App() {
   const [list, setList] = useState(initialData);
+  const [deneme,setDeneme] = useState(productList)
   const [selectedCar, setSelectedCar] = useState(false);
   const navigate = useNavigate();
 
+  console.log(deneme)
+  
   const handleCarClick = (id) => {
     const selectedCar = list.find(car => car.id === id);
     setSelectedCar(selectedCar);
--- src/components/detailPopup/index.js
+++ src/components/detailPopup/index.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
+
 import { productList } from '../../assets/data';
+import './index.css';
 
 function Detail({ setcloseCarPage }) {
   const [vehicle, setVehicle] = useState(productList);
@@ -19,7 +22,33 @@ function Detail({ setcloseCarPage }) {
       </Helmet>
       {vehicle.map(info => (
         <div className='detailVehicle' key={info.productId}>
-          <div className='carYear'>{info.productName}</div>
+          <table>
+            <tbody>
+              <tr>
+                <td colSpan='2'>{info.productName}</td>
+              </tr>
+              <tr>
+                <td>Model</td>
+                <td>{info.model}</td>
+              </tr>
+              <tr>
+                <td>Trim Code</td>
+                <td>{info.trimCode}</td>
+              </tr>
+              <tr>
+                <td>Motor Gücü</td>
+                <td>{info.enginePower}</td>
+              </tr>
+              <tr>
+                <td>Vites Türü</td>
+                <td>{info.transmission}</td>
+              </tr>
+              <tr>
+                <td>Renk Seçenekleri</td>
+                <td>{info.color}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       ))}
       <button onClick={comeBackMainPage}>Anasayfa</button>
