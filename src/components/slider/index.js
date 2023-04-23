b8c116f99e25fcb651bdbe059c80033bcfa52ebf
GoktugYmsk <gogtug033@outlook.com>
2023-04-23 20:52:35 +0300

    css fixed

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 function Slider() {
+
+    const warningMessage = 'https://www.hyundai.com/content/dam/hyundai/tr/tr/images/home/2023/deprem-1860x720-v2.jpg'
+    
     return (
-        <Carousel>
+        <Carousel interval={5000} >
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://www.hyundai.com/content/dam/hyundai/tr/tr/images/home/2022/haziran/one-cikanlar-1-new.jpg"
+                    src={warningMessage}
                     alt="First slide"
-                    style={{ height: '800px' }}
+                    style={{ height: '600px' }}
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -23,7 +25,7 @@ function Slider() {
                     className="d-block w-100"
                     src="https://www.hyundai.com/content/dam/hyundai/tr/tr/images/find-a-car/pip/yeni-i10/one-cikanlar-image-2-new.jpg"
                     alt="Second slide"
-                    style={{ height: '800px', objectFit: 'cover' }}
+                    style={{ height: '600px', objectFit: 'cover' }}
                 />
 
                 <Carousel.Caption>
@@ -36,7 +38,19 @@ function Slider() {
                     className="d-block w-100"
                     src="https://www.hyundai.com/content/dam/hyundai/tr/tr/images/find-a-car/pip/elantra/CN7_FMC_K4_LHD_LIFESTYLE_MODEL_07_CMYK.jpg"
                     alt="Third slide"
-                    style={{ height: '800px' }}
+                    style={{ height: '600px' }}
+                />
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://www.hyundai.com/content/dam/hyundai/tr/tr/images/home/2022/haziran/one-cikanlar-1-new.jpg"
+                    alt="Third slide"
+                    style={{ height: '600px' }}
                 />
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
