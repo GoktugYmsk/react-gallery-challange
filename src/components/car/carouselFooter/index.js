b863a8514e2b951b4ef5983adf63ca9c57d37b89
GoktugYmsk <gogtug033@outlook.com>
2023-04-25 16:17:58 +0300

    footer carousel edited

--- src/components/car/carouselFooter/index.js
+++ src/components/car/carouselFooter/index.js
@@ -1,69 +1,58 @@
 import React, { useState } from 'react';
-import './index.css';
+
+import './index.css'
 
 function Carousel() {
-  const images = [
-    'https://www.hyundai.com/content/dam/hyundai/tr/tr/images/find-a-car/pip/kona/kasim-2022/KONA-new-704x468.jpg',
-    'https://www.hyundai.com/content/dam/hyundai/tr/tr/data/vehicle-thumbnail/product/elantra-2021/default/elantra-672-364.png',
-    'https://www.hyundai.com/content/dam/hyundai/tr/tr/data/vehicle-thumbnail/product/i20n/default/i20n-672x364.png',
-    'https://www.hyundai.com/content/dam/hyundai/tr/tr/images/find-a-car/pip/kona/kasim-2022/KONA-new-704x468.jpg',
-    'https://www.hyundai.com/content/dam/hyundai/tr/tr/images/find-a-car/pip/kona/kasim-2022/KONA-new-704x468.jpg',
-    'https://www.hyundai.com/content/dam/hyundai/tr/tr/images/find-a-car/pip/kona/kasim-2022/KONA-new-704x468.jpg'
+  const [currentPage, setCurrentPage] = useState(0);
+  const pages = [
+    {
+      title: 'Medya Merkezi',
+      content: 'Charge myHyundai, Avrupa’da 500.000 Şarj Noktasına Ulaştı.',
+      date: 'Yerel|2023.04.18',
+    },
+    {
+      title: 'Medya Merkezi',
+      content: 'Hyundai 2030 Yılına Kadar İlk Üç Elektrikli Araç Üreticisinden Biri Olacak.',
+      date: 'Yerel|2023.04.11',
+    },
+    {
+      title: 'Medya Merkezi',
+      content: 'Hyundai IONIQ 6 Dünyada Yılın Otomobili WCOTY Seçildi.',
+      date: 'Yerel|2023.04.06',
+    },
+    {
+      title: 'Medya Merkezi',
+      content: 'Elektrifikasyonda Üst Düzey Performans: Hyundai IONIQ 5 N.',
+      date: 'Yerel|2023.03.30',
+    },
+    {
+      title: 'Medya Merkezi',
+      content: 'Hyundai Elektrikli Araçlar İçin Otomatik Şarj Robotu Geliştirdi.',
+      date: 'Yerel|2023.03.22',
+    }
   ];
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const prev = () => {
-    setCurrentImageIndex(currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1);
+    setCurrentPage(currentPage === 0 ? pages.length - 1 : currentPage - 1);
   };
 
   const next = () => {
-    setCurrentImageIndex(currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1);
+    setCurrentPage(currentPage === pages.length - 1 ? 0 : currentPage + 1);
   };
 
-
   return (
-    <div className="carousel">
-      <button onClick={prev}>&lt;</button>
-      <div className='page1' >
-        <h3>Medya Merkezi</h3>
-        <p>Yerel|2023.04.18</p>
-        <h4>Charge myHyundai, Avrupa’da 500.000 Şarj Noktasına Ulaştı.</h4>
-        <button>Daha Fazla Yükle</button>
-        <button>İncele</button>
+    <div className="carouselFooterPage">
+      <button className='carouselPrew' onClick={prev}>&lt;</button>
+      <div className="pageCarousel">
+        <h3>{pages[currentPage].title}</h3>
+        <br />
+        <p>{pages[currentPage].date}</p>
+        <br />
+        <h5>{pages[currentPage].content}</h5>
       </div>
-      <div className='page2' >
-        <h3>Medya Merkezi</h3>
-        <p>Yerel|2023.04.11</p>
-        <h4>Hyundai 2030 Yılına Kadar İlk Üç Elektrikli Araç Üreticisinden Biri Olacak.</h4>
-        <button>Daha Fazla Yükle</button>
-        <button>İncele</button>
-      </div>
-      <div className='page3' >
-        <h3>Medya Merkezi</h3>
-        <p>Yerel|2023.04.06</p>
-        <h4>Hyundai IONIQ 6 Dünyada Yılın Otomobili WCOTY Seçildi.</h4>
-        <button>Daha Fazla Yükle</button>
-        <button>İncele</button>
-      </div>
-      <div className='page4' >
-        <h3>Medya Merkezi</h3>
-        <p>Yerel|2023.03.30</p>
-        <h4>Elektrifikasyonda Üst Düzey Performans: Hyundai IONIQ 5 N.</h4>
-        <button>Daha Fazla Yükle</button>
-        <button>İncele</button>
-      </div>
-      <div className='page5' >
-        <h3>Medya Merkezi</h3>
-        <p>Yerel|2023.03.22</p>
-        <h4>Hyundai Elektrikli Araçlar İçin Otomatik Şarj Robotu Geliştirdi.</h4>
-        <button>Daha Fazla Yükle</button>
-        <button>İncele</button>
-      </div>
-      <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
-
-      <button onClick={next}>&gt;</button>
+      <button className='carouselNext' onClick={next}>&gt;</button>
     </div>
   );
-};
+}
 
 export default Carousel;
