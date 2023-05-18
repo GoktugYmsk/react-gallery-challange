3efbe57a7124a27c200785cd942d9e32d20425eb
GoktugYmsk <gogtug033@outlook.com>
2023-05-18 20:18:14 +0300

    popup bug fixed

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './index.css';
 
 function Popup({ selectedCar, setSelectedCar }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const popupRef = useRef();
 
   const closePopupModule = () => {
     setSelectedCar(false);
-    navigate('Hyundai')
-  }
+    navigate('/Hyundai');
+  };
+
+  useEffect(() => {
+    const handleEscKeyPress = (event) => {
+      if (event.keyCode === 27) {
+        closePopupModule();
+      }
+    };
+
+    const handleOutsideClick = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        closePopupModule();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscKeyPress);
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscKeyPress);
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [closePopupModule]);
 
   return (
-    <div className='row' >
-      <div className='col-12' >
-        <div className='modul'>
-          <img className='popupİmage' src={selectedCar.image} alt={`Image of ${selectedCar.title}`} />
+    <div className='row'>
+      <div className='col-12'>
+        <div ref={popupRef} className='modul'>
+          <img className='popupImage' src={selectedCar.image} alt={`Image of ${selectedCar.title}`} />
           <h2 className='selectedCarTitle'>{selectedCar.title}</h2>
           <p>{selectedCar.content}</p>
           <button onClick={closePopupModule} className='closePopup'>Çık</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Popup;
